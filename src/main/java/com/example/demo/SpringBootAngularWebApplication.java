4b763d051ac4d3a15719d8a4d1647bef56e5b312
Aditi Hardas <aditi.hardas@gmail.com>
2020-06-23 18:01:57 -0400

    inisial commit

---
+++ src/main/java/com/example/demo/SpringBootAngularWebApplication.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserRepository;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@Configuration
+public class SpringBootAngularWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularWebApplication.class, args);
+	}
+
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+	
+	
+	@Bean
+	CommandLineRunner init(UserRepository userRepository) {
+		return args -> {
+			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+				User user = new User(name, name.toLowerCase() + "@domain.com");
+				userRepository.save(user);
+			});
+			userRepository.findAll().forEach(System.out::println);
+		};
+	}
+}
